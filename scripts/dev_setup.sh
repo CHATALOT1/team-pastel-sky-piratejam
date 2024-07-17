201b7002a499f56afd8b648415ea07d63fe5bf3a
AlteredBeast <axvantia@proton.me>
2024-07-17 20:18:01 +0100

    Revert "cache cargo package lists, remove `--force` flag from cargo commands in scripts"
    This reverts commit 1ff411d3ffabc0ee7a150afd946aae7427e14691. reverted as it did not fix the issue

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rustup target install wasm32-unknown-unknown
 
-cargo install wasm-server-runner 
+cargo install wasm-server-runner --force
 
-cargo install wasm-bindgen-cli 
+cargo install wasm-bindgen-cli --force
