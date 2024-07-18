e4b1c625616749fb50b83c3c3b4b1ad362c9b3aa
Sylvia Mills <me@chatalot.xyz>
2024-07-18 17:58:08 +0100

    Implement `main` function
    - Creates a bevy app with (modified) DefaultPlugins and runs it
    - Added `#![warn(missing_docs)]`
    - Added crate-level documentation containing our 'Game Summary/Pitch'
    - Renamed crate to `phantasmeow` to match the now-decided game name

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+//! Pawsable real-time atmospheric stealth game. You play as a cat that must avoid the light while
+//! exploring a house inhabited by strange light beings, using the items you find to create helpful
+//! tools.
+#![warn(missing_docs)]
+
+use bevy::prelude::*;
+
 fn main() {
     console_error_panic_hook::set_once();
-    todo!();
+
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins.build().set(WindowPlugin {
+        primary_window: Some(Window {
+            fit_canvas_to_parent: true,
+            ..Default::default()
+        }),
+        ..Default::default()
+    }));
+
+    app.run();
 }
