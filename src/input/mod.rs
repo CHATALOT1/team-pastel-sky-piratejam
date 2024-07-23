8f1ad10b8c163f17e8c317b841c8855bd46307cd
Sylvia Mills <me@chatalot.xyz>
2024-07-23 23:04:32 +0100

    Add `HandleInput` Schedule

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -8,5 +8,17 @@
 //! for relevant Issues and PRs.
 use super::*;
 
+/// This [`Schedule`] contains systems that receive and interpret user input. It is run after
+/// [`PreUpdate`] and before [`Update`].
+#[derive(bevy::ecs::schedule::ScheduleLabel, Debug, Clone, PartialEq, Eq, Hash)]
+struct HandleInput;
+
 /// Input Plugin. See [module-level documentation](self).
-pub fn plugin(app: &mut App) {}
+pub fn plugin(app: &mut App) {
+    // Initialize the `HandleInput` schedule and add it to the `MainScheduleOrder`, to ensure that
+    // it is run.
+    app.init_schedule(HandleInput);
+    app.world_mut()
+        .resource_mut::<bevy::app::MainScheduleOrder>()
+        .insert_after(PreUpdate, HandleInput);
+}
