95ac05b12a37ea5b1b7a4242c4ff03136e5665fa
AlteredBeast <axvantia@proton.me>
2024-07-23 21:02:59 +0100

    implement basic movement system
    note this has a bug with loading the texture for the player, after a short amount of time the texture will dissapear

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@
 //! a house inhabited by strange light beings, using the items you find to create helpful tools.
 #![warn(missing_docs)]
 
-use bevy::prelude::*;
+use bevy::{
+    ecs::query,
+    input::common_conditions::input_pressed,
+    math::{vec2, vec3},
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle, Wireframe2dConfig, Wireframe2dPlugin},
+};
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -15,7 +21,60 @@ fn main() {
             ..Default::default()
         }),
         ..Default::default()
-    }));
+    }))
+    .add_systems(Startup, setup)
+    .add_systems(Update, movement)
+    .add_event::<Moving>();
 
     app.run();
+}
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Event)]
+struct Moving;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            // TODO: make this a more fitting texture
+            texture: asset_server.load("IMG_2285.png"),
+            transform: Transform::from_scale(vec3(0.25, 0.25, 1.0)),
+            ..default()
+        },
+        Player,
+    ));
+}
+
+fn movement(
+    mut query: Query<&mut Transform, With<Player>>,
+    keys: Res<ButtonInput<KeyCode>>,
+    mut event: EventWriter<Moving>,
+) {
+    // TODO: make this a custom run condition instead of a check within the system
+    if keys.any_pressed([KeyCode::KeyW, KeyCode::KeyS, KeyCode::KeyD, KeyCode::KeyA]) {
+        let mut movement = vec2(0.0, 0.0);
+
+        if keys.pressed(KeyCode::KeyW) {
+            movement.y += 1.0;
+        }
+        if keys.pressed(KeyCode::KeyS) {
+            movement.y -= 1.0;
+        }
+        if keys.pressed(KeyCode::KeyA) {
+            movement.x -= 1.0;
+        }
+        if keys.pressed(KeyCode::KeyD) {
+            movement.x += 1.0;
+        }
+
+        movement = movement.normalize_or_zero();
+        movement.x *= 5.0;
+        movement.y *= 5.0;
+
+        query.get_single_mut().unwrap().translation += vec3(movement.x, movement.y, 2.0);
+        event.send(Moving);
+    }
 }
